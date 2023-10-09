727f52c5105aea1225f9ecc53a5f925da8721c35
Yahel Muñoz <biped.33@gmail.com>
2023-10-09 11:02:38 -0600

    Added cube Spawn and script for movement

---
+++ Assets/Scripts/InfiniteMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteMovement : MonoBehaviour
+{
+    public int bulletSpeed;
+    public Vector3 bulletPosition;
+    public Transform bulletTransform;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    public Vector3 objectPosition = new Vector3(0, 0, 0);
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(objectToSpawn,objectPosition,Quaternion.identity);
+        }
+    }
+}
